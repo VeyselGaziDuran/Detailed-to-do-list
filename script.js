6652eef81aae01410d62fcb067bfa8611e9de355
Veysel Gazi Duran <veyselduran82@gmail.com>
2023-07-22 01:13:16 +0300

    JavaScript
    Added JavaScript code to create a simple TODO list app with features like adding new TODO items, marking items as completed, and removing items from the list.

---
+++ script.js
@@ -0,0 +1,50 @@
+//! Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const todoFilter = document.querySelector(".filter");
+
+//! Alerts
+const alertWarning = document.querySelector(".alert-warning");
+const alertSuccess = document.querySelector(".alert-success");
+
+//! Functions
+function addTodo(e) {
+    e.preventDefault();
+
+    // creat todo div
+    const todoDIv = document.createElement("div");
+    todoDIv.classList.add("todo")
+
+    // check mark button
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = "<i class='fas fa-check-circle'></i>";
+    completedButton.classList.add("complete-btn");
+    todoDIv.appendChild(completedButton);
+
+    // creat todo li
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDIv.appendChild(newTodo);
+
+    //check trash button
+    const trasButton = document.createElement("button");
+    trasButton.innerHTML = "<i class='fa fa-minus-circle'></i>";
+    trasButton.classList.add("tresh-btn");
+    todoDIv.appendChild(trasButton);
+
+    // append to list
+    todoList.appendChild(todoDIv);
+
+    // clear todo input value
+    todoInput.value = "";
+
+  }
+
+//! Events
+todoButton.addEventListener("click", addTodo);
+
+
+
+
